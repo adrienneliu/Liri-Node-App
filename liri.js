d8ce16564941ea683f15e56643631ededef6650a
Adrienne <iliu.adrienne@gmail.com>
2019-06-19 21:28:43 -0700

    add axios bands in town, no functions yet

--- liri.js
+++ liri.js
@@ -32,43 +32,56 @@ var axios = require("axios");
 //     }
 // })
 
-var movieTitle = process.argv.slice(2).join("+");
-//var one = 1; 
-console.log(movieTitle);
+// var movieTitle = process.argv.slice(2).join("+");
+// //var one = 1; 
+// console.log(movieTitle);
 
-axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy")
-.then(function(response) {
+// axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy")
+// .then(function(response) {
 
-    console.log("Title: " + response.data.Title +
-    "\nYear released: " + response.data.Released + 
-    "\nIMBD rating: " + response.data.imdbRating +
-    //"\nRotten Tomato rating: " + response.data.Ratings + 
-    "\nCountry produced: " + response.data.Country + 
-    "\nLanguage: " + response.data.Language +
-    "\nPlot: " + response.data.Plot + 
-    "\nActors: " + response.data.Actors)
-    //"\nRotten Tomato rating: " + response.data.Ratings[one].Value 
-})
+//     console.log("Title: " + response.data.Title +
+//     "\nYear released: " + response.data.Released + 
+//     "\nIMBD rating: " + response.data.imdbRating +
+//     "\nRotten Tomato rating: " + response.data.Ratings + 
+//     "\nCountry produced: " + response.data.Country + 
+//     "\nLanguage: " + response.data.Language +
+//     "\nPlot: " + response.data.Plot + 
+//     "\nActors: " + response.data.Actors)
+//     //"\nRotten Tomato rating: " + response.data.Ratings[one].Value 
+// })
 
-.catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+// .catch(function(error) {
+//     if (error.response) {
+//       // The request was made and the server responded with a status code
+//       // that falls out of the range of 2xx
+//       console.log("---------------Data---------------");
+//       console.log(error.response.data);
+//       console.log("---------------Status---------------");
+//       console.log(error.response.status);
+//       console.log("---------------Status---------------");
+//       console.log(error.response.headers);
+//     } else if (error.request) {
+//       // The request was made but no response was received
+//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//       console.log(error.request);
+//     } else {
+//       // Something happened in setting up the request that triggered an Error
+//       console.log("Error", error.message);
+//     }
+//     console.log(error.config);
+//   });
 
 // "\nRotten Tomato rating: " + response.data.Ratings[1].Value 
+
+var artist = process.argv.slice(2).join("+");
+console.log(artist);
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+.then(function(response) {
+//console.log(response);
+console.log(response.data[0].lineup)
+console.log("Venue name: " + response.data[0].venue.name +
+"\nVenue location: " + response.data[0].venue.city + " , " + response.data[0].venue.country +
+"\nVenue date: " + response.data[0].datetime)
+
+})
