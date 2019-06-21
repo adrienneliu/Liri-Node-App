deb0c72d91675f4fda83e2fa3348705549c385f5
Adrienne <iliu.adrienne@gmail.com>
2019-06-20 23:46:38 -0700

    add do-what-it-says function

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 //required packages
 var keys = require("./keys.js");
-console.log(keys.spotify)
+// console.log(keys.spotify)
 //spotify api in node
 var Spotify = require('node-spotify-api');
 //our own keys we've defined
@@ -11,122 +11,169 @@ var spotify = new Spotify(keys.spotify);
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require("moment");
-
+var fs = require("fs");
 //optional packages
 var chalk = require("chalk");
 
-//ask the user to select an option 
-// inquirer.prompt ([
-// {
-//     type: "list", 
-//     message: "What do you want to do first?", 
-//     choices: ["concert-this","spotify-this-song", "movie-this", "do-what-it-says"],
-//     name: "category" } 
-// ]).then(function(response){
-//     if (response.category === "concert-this") {
-//         process.argv[2] = "concert-this";
-//         console.log("hey!");
-//     }
-//     if (response.category === "spotify-this-song") {
-//         console.log("Why!"); 
-//     } 
-//     if (response.category === "movie-this") {
-//         console.log("No movies for you..."); 
-//     }
-//     if (response.category === "do-what-it-says") {
-//         console.log("I REFUSE!");
-//     }
-// })
 
 
-//type in the title to get more details
-// var movieTitle = process.argv.slice(2).join("+");
-// //var one = 1; 
-// console.log(movieTitle);
+//puts the choices in array for easy identification
+var selector = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
 
-// axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy")
-// .then(function(response) {
+var selectMe = function () {
+  //ask the user to select an option 
 
-//     console.log(chalk.green("Title: " + response.data.Title +
-//     "\nYear released: " + response.data.Released + 
-//     "\nIMBD rating: " + response.data.imdbRating +
-//     "\nRotten Tomato rating: " + response.data.Ratings + 
-//     "\nCountry produced: " + response.data.Country + 
-//     "\nLanguage: " + response.data.Language +
-//     "\nPlot: " + response.data.Plot + 
-//     "\nActors: " + response.data.Actors))
-//     //"\nRotten Tomato rating: " + response.data.Ratings[one].Value 
-// })
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What do you want to do?",
+      choices: selector,
+      name: "category"
+    }
+  ]).then(function (response) {
+    if (response.category === selector[0]) {
 
-// .catch(function(error) {
-//     if (error.response) {
-//       // The request was made and the server responded with a status code
-//       // that falls out of the range of 2xx
-//       console.log("---------------Data---------------");
-//       console.log(error.response.data);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.status);
-//       console.log("---------------Status---------------");
-//       console.log(error.response.headers);
-//     } else if (error.request) {
-//       // The request was made but no response was received
-//       // `error.request` is an object that comes back with details pertaining to the error that occurred.
-//       console.log(error.request);
-//     } else {
-//       // Something happened in setting up the request that triggered an Error
-//       console.log("Error", error.message);
-//     }
-//     console.log(error.config);
-//   });
+      bandsinTown();
+    }
+    else if (response.category === selector[1]) {
+      spotifyMe();
+    }
+    else if (response.category === selector[2]) {
+      moviesOMBD();
 
-// "\nRotten Tomato rating: " + response.data.Ratings[1].Value 
+    }
+    else if (response.category === selector[3]) {
+      dowhatitSays();
+    }
+  });
+}
+//run this first
+selectMe();
 
+
+var moviesOMBD = function () {
+
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What movie do you want to see?",
+      name: "usermovie"
+
+    }
+  ]).then(function (responseMovie) {
+    if (responseMovie.usermovie === "") {
+      console.log("\n-----------------------\n");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!\n");
+
+    } else {
+      if (responseMovie.usermovie) {
+        var queryURL = "http://www.omdbapi.com/?t=" + responseMovie.usermovie.split(" ").join("+") + "&y=&plot=short&apikey=trilogy"
+      }
+      axios.get(queryURL)
+        .then(function (response) {
+
+          console.log(chalk.magenta("\nTitle: " + response.data.Title +
+            "\nYear released: " + response.data.Released +
+            "\nIMBD rating: " + response.data.imdbRating +
+            "\nRotten Tomato rating: " + response.data.Ratings[0].Value +
+            "\nCountry produced: " + response.data.Country +
+            "\nLanguage: " + response.data.Language +
+            "\nPlot: " + response.data.Plot +
+            "\nActors: " + response.data.Actors + "\n\n"))
+        })
+    }
+    selectMe();
+
+  })
+}
 
 
 //get venue details from artists and bands
-// var artist = process.argv.slice(2).join("+");
-// console.log(artist);
+var bandsinTown = function () {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What band do you want to see?",
+      name: "userband"
+    }
+  ]).then(function (responseBand) {
+    if (responseBand.userband) {
+      var queryURL = "https://rest.bandsintown.com/artists/" + responseBand.userband + "/events?app_id=codingbootcamp"
+    }
+    axios.get(queryURL)
+      .then(function (response) {
+        //console.log(responseResult);
 
-// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-//     .then(function (response) {
-//         //console.log(response);
+        if (response.data.length === 0 || response.data.length === "") {
+          console.log("\nPlease pick something else\n");
 
-//         for (var i = 0; i <= 3; i++) {
+        } else {
+          for (var i = 0; i <= 1; i++) {
 
-//             console.log(response.data[i].lineup)
-//             console.log(chalk.blue("Venue name: " + response.data[i].venue.name +
-//                 "\nVenue location: " + response.data[i].venue.city + " , " + response.data[i].venue.country +
-//                 "\nVenue date: " + (chalk.red(moment(response.data[i].datetime).format('L')))))
+            // console.log(responseResult.data[i].lineup)
+            console.log(chalk.blue("\nVenue name: " + response.data[i].venue.name +
+              "\nVenue location: " + response.data[i].venue.city + " , " + response.data[i].venue.country +
+              "\nVenue date: "
+              + (chalk.red(moment(response.data[i].datetime).format('L')))));
+          }
+        } selectMe();
+      });
+  })
 
-//     }
-//     });
-
-
+}
 
 //spotify 
-
 var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret
 });
 
-spotify.search({ type: 'track', query: "we're finally landing",  limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+var spotifyMe = function () {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What song do you want to see?",
+      name: "usersong"
     }
-    //console.log(data.tracks.items)
-    console.log("Artist Name: " + data.tracks.items[0].artists[0].name)
-    console.log("Song Name: " + data.tracks.items[0].name)
-    console.log("Preview Link: " + data.tracks.items[0].preview_url)
-    console.log("Album: " + data.tracks.items[0].album.name)
+  ]).then(function (response) {
+    if (response.usersong === "") {
+      spotify.search({ type: 'track', query: "The Sign", query: "Ace of Base", limit: 3 }, function (err, data) {
+        console.log(chalk.yellow("\n\nArtist Name: ") + data.tracks.items[0].artists[0].name +
+          (chalk.yellow("\nSong Name: ")) + data.tracks.items[0].name +
+          (chalk.yellow("\nSong Name: ")) + data.tracks.items[0].name +
+          (chalk.yellow("\nPreview Link: ")) + data.tracks.items[0].preview_url +
+          (chalk.yellow("\nAlbum: ")) + data.tracks.items[0].album.name + "\n\n")
 
-    // console.log(data.tracks.items.album.href)
+        }  
+      )}else{
+    spotify.search({ type: 'track', query: "'" + response.usersong + "'", limit: 3 }, function (err, data) {
+      console.log(chalk.yellow("\nArtist Name: ") + data.tracks.items[0].artists[0].name +
+        (chalk.yellow("\nSong Name: ")) + data.tracks.items[0].name +
+        (chalk.yellow("\nSong Name: ")) + data.tracks.items[0].name +
+        (chalk.yellow("\nPreview Link: ")) + data.tracks.items[0].preview_url +
+        (chalk.yellow("\nAlbum: ")) + data.tracks.items[0].album.name + "\n\n"
+      )
 
-  // console.log("Session: %j", data);
-//   console.log('----A-------'); 
-//   console.log(data.tracks);
-//   console.log('-----B------');
-//   console.log(data.tracks.items);
-//   console.log('-----C------');
-//   console.log(data.tracks.artists);
+    })
+  }selectMe();
+  })
+  
+}
+
+
+var dowhatitSays = function () {
+
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    var dataArray = data.split(",");
+    for (var i = 0; i < dataArray.length; i++) {
+      console.log(dataArray[i] + "\n");
+    }
+    // console.log(dataArray);
+
+    selectMe();
   });
+};
